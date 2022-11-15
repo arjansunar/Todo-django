bbbb2af933cf5626ddd4ccbec3b8f07f356cecf3
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-11-15 13:02:34 +0545

    using drf

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-from django.http import HttpResponse
 # Create your views here.
+@api_view(['GET'])
 def index(request): 
-    return HttpResponse('hi there')
+    return Response({"message": "hello"})
+
+
