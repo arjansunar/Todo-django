c32864078d40b05b3d8cef42f800a67c74a95acc
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-11-19 06:35:47 +0545

    adding logout button

--- todo-react/src/App.tsx
+++ todo-react/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Login, Signup, TaskListContainer } from "./pages";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Error, LogoutButton } from "./components";
 import { ProtectedRoutes } from "./routing/ProtectedRoutes";
 import { useContext } from "react";
@@ -15,10 +15,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route element={<ProtectedRoutes />}>
           <Route path="/task" element={<TaskListContainer />} />
           <Route path="/test" element={<div>test</div>} />
-          {/* <Route path="/" element={<div>Home</div>} /> */}
         </Route>
         <Route path="*" element={<Error message="Page not found" />} />
       </Routes>
